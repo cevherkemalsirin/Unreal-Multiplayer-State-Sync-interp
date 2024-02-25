a93cca1db8574976a96ad8dd76a03f63c1474c78
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-02-26 01:09:18 +0300

    Created Camera and Steering systems

--- Source/multiHub/Private/Pawns/RayCarBase.cpp
+++ Source/multiHub/Private/Pawns/RayCarBase.cpp
@@ -3,9 +3,12 @@
 
 #include "Pawns/RayCarBase.h"
 #include "Pawns/Components/RayWheel.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
-ARayCarBase::ARayCarBase():wheelNumber(4), drawDebug(false)
+ARayCarBase::ARayCarBase() 
+	:wheelNumber(4), drawDebug(false), currentCamIndex(0), maxWheelAngle(45.0f), currentWheelAngle(0.0f)
 {
  
 	PrimaryActorTick.bCanEverTick = true;
@@ -13,6 +16,9 @@ ARayCarBase::ARayCarBase():wheelNumber(4), drawDebug(false)
 	bodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ChasisMesh"));
 	this->SetRootComponent(bodyMesh);
 	
+	steeringWheelMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("steeringMesh"));
+	steeringWheelMesh->SetupAttachment(RootComponent);
+
 	topLink_RF = CreateDefaultSubobject<USceneComponent>(TEXT("topLinkRF"));
 	topLink_LF = CreateDefaultSubobject<USceneComponent>(TEXT("ToplinkLF"));
 	topLink_RB = CreateDefaultSubobject<USceneComponent>(TEXT("ToplinkRB"));
@@ -36,22 +42,44 @@ ARayCarBase::ARayCarBase():wheelNumber(4), drawDebug(false)
 	wheel_LB = CreateDefaultSubobject <URayWheel>(TEXT("Wheel_LB"));
 	wheel_LB->SetupAttachment(topLink_LB);
 
+	//Init cameras
+	innerCam = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera_1"));
+	outerCam = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera_2"));
+	innerCamBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("cameraBoom_1"));
+	outerCamBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("cameraBoom_2"));
+
+	innerCamBoom->SetupAttachment(RootComponent);
+	outerCamBoom->SetupAttachment(RootComponent);
+
+	cameras.Add(outerCam);
+	cameras.Add(innerCam);
+	cameraBooms.Add(outerCamBoom);
+	cameraBooms.Add(innerCamBoom);
+
+
+	innerCam->SetupAttachment(innerCamBoom);
+	outerCam->SetupAttachment(outerCamBoom);
 }
 
 
 void ARayCarBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	for (int i = 0; i < wheelNumber; i++)
 	{
-		rayLengths.Add(suspensionProperties[i].restLength  + suspensionProperties[i].travel + wheel_RF->GetRadius());
+		rayLengths.Add(suspensionProperties[i].restLength + suspensionProperties[i].travel + wheel_RF->GetRadius());
 		//Init default values
 		suspensionLengths.Add(suspensionProperties[i].restLength);
 		PreSuspensionLengths.Add(suspensionProperties[i].restLength);
 		b_wheelsContact.Add(false);
 		hitResults.Add(FHitResult());
 		suspensionFPower.Add(0.0f);
+	}
+
+	for (const auto& cam : cameraBooms)
+	{
+		camerasStartRots.Add(cam->GetRelativeRotation());
 	}
 
 }
@@ -141,9 +169,88 @@ void ARayCarBase::ApplySuspensionForce(uint32 i)
 }
 
 
+void ARayCarBase::ResetCameras()
+{
+	for (int i = 0; i < cameraBooms.Num(); i++)
+	{
+		cameraBooms[i]->SetRelativeRotation(camerasStartRots[i]);
+	}
+	GetController()->SetControlRotation(FRotator(0.f, 0.f, 0.f));
+
+}
+
+
+void ARayCarBase::SwapCameras()
+{
+	if (currentCamIndex == cameras.Num() - 1)
+	{
+		currentCamIndex = 0;
+	}
+	else
+	{
+		currentCamIndex++;
+	}
+
+	for (uint8 i = 0; i < cameras.Num(); i++)
+	{
+		ResetCameras();
+		if (i == currentCamIndex)
+		{
+			cameras[i]->SetActive(true);
+		}
+		else
+		{
+			cameras[i]->SetActive(false);
+		}
+	}
+	
+}
+
+
+void ARayCarBase::Steer(float value)
+{
+	float wheelAngle = 0.0f;
+	if (value)
+	{
+		currentWheelAngle += value * deltaTime * 100.0f;
+		currentWheelAngle = FMath::Clamp(currentWheelAngle, -maxWheelAngle, maxWheelAngle);
+
+	}
+	else
+	{
+		currentWheelAngle *= 0.93f; //make wheels reset to 0 when release input
+	}
+	wheelAngle = (currentWheelAngle / maxWheelAngle) * 180.0f;
+	topLink_LF->SetRelativeRotation(FRotator(topLink_LF->GetRelativeRotation().Pitch, currentWheelAngle, topLink_LF->GetRelativeRotation().Roll));
+	topLink_RF->SetRelativeRotation(FRotator(topLink_RF->GetRelativeRotation().Pitch, currentWheelAngle, topLink_RF->GetRelativeRotation().Roll));
+	steeringWheelMesh->SetRelativeRotation(
+		FRotator(steeringWheelMesh->GetRelativeRotation().Pitch,
+			steeringWheelMesh->GetRelativeRotation().Yaw, wheelAngle));
+}
+
+
+void ARayCarBase::LookUp(float value)
+{
+	AddControllerPitchInput(value);
+}
+
+
+void ARayCarBase::TurnRight(float value)
+{
+	AddControllerYawInput(value);
+}
+
+
 void ARayCarBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (PlayerInputComponent)
+	{
+		PlayerInputComponent->BindAction("SwapCamera", EInputEvent::IE_Pressed, this, &ARayCarBase::SwapCameras);
+		PlayerInputComponent->BindAxis("LookUp", this, &ARayCarBase::LookUp);
+		PlayerInputComponent->BindAxis("TurnRight", this, &ARayCarBase::TurnRight);
+		PlayerInputComponent->BindAxis("MoveRight", this, &ARayCarBase::Steer);
+	}
 }
 
--- Source/multiHub/Public/Pawns/RayCarBase.h
+++ Source/multiHub/Public/Pawns/RayCarBase.h
@@ -8,6 +8,8 @@
 #include "RayCarBase.generated.h"
 
 class URayWheel;
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class MULTIHUB_API ARayCarBase : public APawn
@@ -22,7 +24,13 @@ class MULTIHUB_API ARayCarBase : public APawn
 	// length of ray trace from at the begining of suspension to the bottom of the wheel. For each wheels
 	TArray<float> rayLengths;
 
+	TArray<UCameraComponent*> cameras;
+	TArray<USpringArmComponent*> cameraBooms;
+	TArray<FRotator> camerasStartRots;
 
+	uint8 currentCamIndex;
+	
+	
 
 	//Casting ray from each suspensions to end of the wheel
 	/*
@@ -34,10 +42,19 @@ class MULTIHUB_API ARayCarBase : public APawn
 
 	void ApplySuspensionForce(uint32 targetIndex);
 
+	void ResetCameras();
+
+	//input related
+	void Steer(float value);
+	void LookUp(float value);
+	void TurnRight(float value);
+
 protected:
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly ,Category = "Vehicle", meta=(AllowPrivateAcess = "true"))
 	UStaticMeshComponent* bodyMesh;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle", meta = (AllowPrivateAcess = "true"))
+	UStaticMeshComponent* steeringWheelMesh;
 	//Suspension placements.
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Suspension", meta = (AllowPrivateAcess = "true"))
 	USceneComponent* topLink_RF;
@@ -84,9 +101,27 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Vehicle | Suspension", meta = (AllowPrivateAcess = "true"))
 	TArray<FSuspension> suspensionProperties;
 	
+	//cameras
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Camera", meta = (AllowPrivateAcess = "true"))
+	UCameraComponent* innerCam;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Camera", meta = (AllowPrivateAcess = "true"))
+	UCameraComponent* outerCam;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Camera", meta = (AllowPrivateAcess = "true"))
+	USpringArmComponent* innerCamBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Camera", meta = (AllowPrivateAcess = "true"))
+	USpringArmComponent* outerCamBoom;
+	
+
+	//steering
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Vehicle | Steering", meta = (AllowPrivateAcess = "true"))
+	float maxWheelAngle;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle | Steering", meta = (AllowPrivateAcess = "true"))
+	float currentWheelAngle;
 
 	float deltaTime;
 	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintCallable, Category = "Vehicle | Camera")
+	void SwapCameras();
 
 public:	
 
